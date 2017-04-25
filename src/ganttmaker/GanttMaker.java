ccdb06644e6207ed6252f9b8d04750e73e43b7f5
blaze <blaze@DESKTOP-AHIUUHC>
2017-04-25 17:36:30 +0200

    core

--- src/ganttmaker/GanttMaker.java
+++ src/ganttmaker/GanttMaker.java
@@ -10,20 +10,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
-import javafx.geometry.Orientation;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -57,43 +52,29 @@ public class GanttMaker extends Application  {
         Label textName = new Label("Wpisz nazwe zadania");
         Label textDlugosc = new Label("Wpisz dlugosc");
         Label textType = new Label("Wybierz typ czasu");
-        Label error = new Label("Error");
-        error.setVisible(false);
-        error.setScaleX(3);
-        error.setScaleY(3);
-        error.setTranslateY(100);
-        error.setTranslateX(-100); 
         
         Button buttonOk = new Button("Dodaj Zadanie");
         Button buttonAll = new Button("Wypisz Wsio");
         Button buttonMakeXML = new Button("Stworz XML");
         Button buttonTakeXML = new Button("Odtworz XML");
         Button buttonCreateChart = new Button("Rysuj Wykres");
-        /*
-        ScrollBar sc = new ScrollBar();
-        sc.setMin(0);
-        sc.setMax(100);
-        sc.setValue(50);
-        sc.setOrientation(Orientation.HORIZONTAL);
-        */
+
         Tasks tasks = new Tasks();
         tasks.setTasks(new ArrayList<>());
         
         BorderPane mainPane = new BorderPane();
         
-    //    List<Task> taskList = new ArrayList<Task>();
+
         
         EventHandler<javafx.scene.input.MouseEvent> addTaskEvent = 
         new EventHandler<javafx.scene.input.MouseEvent>() { 
         @Override 
         public void handle(javafx.scene.input.MouseEvent e) { 
-                //ObservableList<String> types;
-                //types = typeList.getSelectionModel().getSelectedItems();
-                error.setVisible(false);
+              
               
                 try {
                 
-                //int dlInt = Integer.parseInt(wpiszDlugosc.getText());
+                
                String types = typeList.getSelectionModel().getSelectedItem();
                     
                Task tasktemp = new Task();
@@ -191,11 +172,7 @@ public class GanttMaker extends Application  {
                        taskTemp.setTypeOfLength(tasksm.getTasks().get(taskmIndex).getTypeOfLength());
                        tasks.getTasks().add(taskTemp);
                        System.out.println(tasks.getTasks().size());
-                       //tasks.getTasks().add(new Task());
-                       //tasks.getTasks().get(listSize).setLength(tsk.getLength()); 
-                       //tasks.getTasks().get(listSize).setName(tsk.getName());
-                       //tasks.getTasks().get(listSize).setTypeOfLength(tsk.getTypeOfLength());
-                       //System.out.println(listSize); 
+                
                        }
                    }
                    else if (listSize > 0)
@@ -220,13 +197,7 @@ public class GanttMaker extends Application  {
                    System.out.println("Nie ma nic do dodania");
                }
                else System.out.println("Nieoczekiwany blad");
-                // get size-1  (juz dodalismy wiec jest ok)
-                // funkcja ktora sprawdzi czy w tasksm i tasks jest to samo
-                // jesli tasksm jest pusty (if na caly kod) - nic nie robimy
-                // jesli tasks jest pusty - dodajemy wszysrko - bez sprawdzania funkcją
-                // jesli nie są puste - funkcja sprawdza czy nie sa takie same -
-                // jesli nie sa to dodaje wszystko, jesli są, nic sie nie wykona
-               
+     
                 } catch (JAXBException ex) {
                 System.out.println("Jakis problem");
                 }
@@ -235,64 +206,56 @@ public class GanttMaker extends Application  {
  
         //StackPane stack = new StackPane();
         AnchorPane anchorPane = new AnchorPane();
+        
+        ScrollPane scrollpane = new ScrollPane(anchorPane);
+        scrollpane.setPrefSize(600, 200);
+        
         //ObservableList list = anchorPane.getChildren();  
         EventHandler<javafx.scene.input.MouseEvent> createChart = 
         new EventHandler<javafx.scene.input.MouseEvent>() { 
         @Override 
         public void handle(javafx.scene.input.MouseEvent e) { 
                
-               //Group rectangles = new Group();
-               //Group text = new Group();
-               //Pane rectPane = new Pane();
+          
                int x=100,y=100;
-               int width;
+               int width=0;
                int height=20;
                anchorPane.getChildren().clear();
-               //ArrayList<Rectangle> rectList = new ArrayList<>();
-               //ArrayList<Text> textList = new ArrayList<>();
-               //rectList.clear();
-               //textList.clear();
-               //text.getChildren().removeAll(textList);
-               //rectangles.getChildren().removeAll(rectList);
-               
-               
+     
                int tasksSize = tasks.getTasks().size();
                for(int i=0;i<tasksSize;i++)
                {   
-                width=tasks.getTasks().get(i).getLength();
+                   
+                if("Minut".equals(tasks.getTasks().get(i).getTypeOfLength()))
+                {
+                    width=tasks.getTasks().get(i).getLength();
+                }
+                else if("Godzin".equals(tasks.getTasks().get(i).getTypeOfLength()))
+                {
+                     width=tasks.getTasks().get(i).getLength()*60;
+                }
+                else if("Dni".equals(tasks.getTasks().get(i).getTypeOfLength()))
+                {
+                    width=tasks.getTasks().get(i).getLength()*60*24;
+                }
+                   
+               // width=tasks.getTasks().get(i).getLength();
                 Rectangle rect = new Rectangle(x,y,width,height);
                 rect.setFill(Color.CADETBLUE);
                 anchorPane.getChildren().add(rect);
                 
                 anchorPane.getChildren().add(new Text(x+width,y+15,tasks.getTasks().get(i).getName()+ " - " + tasks.getTasks().get(i).getLength()+ " " + tasks.getTasks().get(i).getTypeOfLength()));
                    
-                //textList.add(new Text(,y,tasks.getTasks().get(i).getName()));
-               // textList.get(i).setX(rectList.get(i).getX());
-              //  textList.get(i).setY(y);   
-                
+
                 y=y+20;    
                 x=x+width;
                }
-               
-            //   textList.forEach((tex) -> {
-            //       text.getChildren().add(tex);
-            //   });
-               
-           //    rectList.forEach((rec) -> {
-           //        rec.setFill(Color.RED);
-           //        rectangles.getChildren().add(rec);
-            //    });
-               
-               
-               //stack.getChildren().addAll(rectangles,text);
-               mainPane.setCenter(anchorPane);
+            mainPane.setCenter(scrollpane);
+              
                
         }
         };
-        
-        
-        
-        
+
         buttonOk.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, addTaskEvent);
         buttonAll.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, checkTasksEvent);
         buttonMakeXML.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, createXML);
@@ -300,12 +263,10 @@ public class GanttMaker extends Application  {
         buttonCreateChart.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, createChart);
         
         VBox vBoxLeft = new VBox(textName,wpiszNazwe,textDlugosc,wpiszDlugosc,textType,typeList,buttonOk,buttonAll,buttonMakeXML,buttonTakeXML,buttonCreateChart);
-        VBox vBoxRight = new VBox(error);
+        //VBox vBoxRight = new VBox();
 
         
         mainPane.setLeft(vBoxLeft);
-        mainPane.setCenter(vBoxRight);
-        
 
         Scene scene = new Scene(mainPane, 300, 250);
         
